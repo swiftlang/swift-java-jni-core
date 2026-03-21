8273e74cc56b3176e914e37408446c3ebb4007ad
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-03-21 19:03:28 -0400

    Export `JavaDemanglingError` as public

--- Sources/SwiftJavaJNICore/JavaDemanglingError.swift
+++ Sources/SwiftJavaJNICore/JavaDemanglingError.swift
@@ -13,7 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 /// Describes an error that can occur when demangling a Java name.
-enum JavaDemanglingError: Error {
+public enum JavaDemanglingError: Error {
   /// This does not match the form of a Java mangled type name.
   case invalidMangledName(String)
 
